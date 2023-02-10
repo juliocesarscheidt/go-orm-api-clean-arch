a70f9ef4ffc88eb0a26166e65ab2db7b07b1a9c2
juliocesarscheidt <julio.scheidt96@gmail.com>
2023-02-10 19:35:49 -0300

    Change readme

--- quality-gate/run.sh
+++ quality-gate/run.sh
@@ -3,18 +3,58 @@
 export SRC_PATH="${PWD}/../"
 
 #  max virtual memory areas vm.max_map_count [65530] is too low, increase to at least [262144]
-# echo 'vm.max_map_count=262144' >> /etc/sysctl.conf
-# sysctl -p
+# vim /etc/sysctl.conf
+# vm.max_map_count=262144
+# fs.file-max=65536
+
+# sysctl -w vm.max_map_count=262144
 # sysctl vm.max_map_count
+# sysctl -w fs.file-max=65536
+# sysctl fs.file-max
+
+# sysctl -w vm.overcommit_memory=2
+# sysctl -w vm.overcommit_ratio=100
+
+# ulimit -n 131072
+# ulimit -u 8192
 
 ########################### sonarqube ###########################
-docker container run --rm \
+docker container run --rm -it --stop-timeout 3600 \
   --name sonarqube \
   --network bridge \
   -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE='true' \
-  -p 9000:9000 \
-  --memory='512MB' --cpus='0.5' \
+  -p 9000:9000 -p 9001:9001 -p 9092:9092 \
+  -v sonarqube-conf:/opt/sonarqube/conf \
+  -v sonarqube-data:/opt/sonarqube/data \
+  -v sonarqube-logs:/opt/sonarqube/logs \
+  -v sonarqube-extensions:/opt/sonarqube/extensions \
   sonarqube:lts sh 2> /dev/null
+
+# cat /var/lib/docker/volumes/sonarqube-conf/_data/sonar.properties
+
+# /opt/java/openjdk/bin/java -version
+# /opt/java/openjdk/bin/java -Xmx1024m -Xms1024m -XX:MaxDirectMemorySize=1024m -XX:+HeapDumpOnOutOfMemoryError -XX:MaxHeapSize=1024m -XX:HeapBaseMinAddress=1024m -version
+
+
+# There is insufficient memory for the Java Runtime Environment to continue.
+# Cannot create worker GC thread. Out of system resources.
+# Possible reasons:
+#   The system is out of physical RAM or swap space
+#   The process is running with CompressedOops enabled, and the Java Heap may be blocking the growth of the native heap
+# Possible solutions:
+#   Reduce memory load on the system
+#   Increase physical memory or swap space
+#   Check if swap backing store is full
+#   Decrease Java heap size (-Xmx/-Xms)
+#   Decrease number of Java threads
+#   Decrease Java thread stack sizes (-Xss)
+#   Set larger code cache with -XX:ReservedCodeCacheSize=
+#   JVM is running with Zero Based Compressed Oops mode in which the Java heap is
+#     placed in the first 32GB address space. The Java Heap base address is the
+#     maximum limit for the native heap growth. Please use -XX:HeapBaseMinAddress
+#     to set the Java Heap base and to place the Java Heap above 32GB virtual address.
+# This output file may be truncated or incomplete.
+#
 
 # ./docker/entrypoint.sh
 # /opt/java/openjdk/bin/java -jar lib/sonarqube.jar \
